aa1d6ee52873f8d8db50e94b660717074a17a477
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2022-05-07 17:36:42 +0800

    Add folly F14 benchmark.

--- src/common/base/test/JoinBenchMark.cpp
+++ src/common/base/test/JoinBenchMark.cpp
@@ -3,6 +3,7 @@
 // This source code is licensed under Apache 2.0 License.
 
 #include <folly/Benchmark.h>
+#include <folly/container/F14Map.h>
 #include <folly/init/Init.h>
 
 #include <cstddef>
@@ -21,6 +22,21 @@ BENCHMARK(std_1k, iters) {
   constexpr std::size_t kData = 1000;
   for (auto i = 0u; i < iters; i++) {
     std::unordered_map<std::size_t, std::size_t> t;
+    t.reserve(kData);
+    for (std::size_t j = 0; j < kData; j++) {
+      t[j] = j;
+    }
+    for (std::size_t j = 0; j < kData; j++) {
+      auto found = t.find(j);
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
+BENCHMARK_RELATIVE(F14_1k, iters) {
+  constexpr std::size_t kData = 1000;
+  for (auto i = 0u; i < iters; i++) {
+    folly::F14FastMap<std::size_t, std::size_t> t(kData);
     t.reserve(kData);
     for (std::size_t j = 0; j < kData; j++) {
       t[j] = j;
@@ -63,6 +79,21 @@ BENCHMARK(std_10k, iters) {
   }
 }
 
+BENCHMARK_RELATIVE(F14_10k, iters) {
+  constexpr std::size_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    folly::F14FastMap<std::size_t, std::size_t> t;
+    t.reserve(kData);
+    for (std::size_t j = 0; j < kData; j++) {
+      t[j] = j;
+    }
+    for (std::size_t j = 0; j < kData; j++) {
+      auto found = t.find(j);
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
 BENCHMARK_RELATIVE(JoinHashTable_10k, iters) {
   constexpr std::size_t kData = 10000;
   for (auto i = 0u; i < iters; i++) {
@@ -83,6 +114,21 @@ BENCHMARK(std_10k_value_int, iters) {
   constexpr int64_t kData = 10000;
   for (auto i = 0u; i < iters; i++) {
     std::unordered_map<Value, std::vector<const List*>> t;
+    t.reserve(kData);
+    for (int64_t j = 0; j < kData; j++) {
+      t[Value(j)].emplace_back(nullptr);
+    }
+    for (int64_t j = 0; j < kData; j++) {
+      auto found = t.find(Value(j));
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
+BENCHMARK_RELATIVE(F14_10k_value_int, iters) {
+  constexpr int64_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    folly::F14FastMap<Value, std::vector<const List*>> t;
     t.reserve(kData);
     for (int64_t j = 0; j < kData; j++) {
       t[Value(j)].emplace_back(nullptr);
@@ -125,6 +171,21 @@ BENCHMARK(std_10k_value_string, iters) {
   }
 }
 
+BENCHMARK_RELATIVE(F14_10k_value_string, iters) {
+  constexpr int64_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    folly::F14FastMap<Value, std::vector<const List*>> t;
+    t.reserve(kData);
+    for (int64_t j = 0; j < kData; j++) {
+      t[Value(std::to_string(j))].emplace_back(nullptr);
+    }
+    for (int64_t j = 0; j < kData; j++) {
+      auto found = t.find(Value(std::to_string(j)));
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
 BENCHMARK_RELATIVE(JoinHashTable_10k_value_string, iters) {
   constexpr int64_t kData = 10000;
   for (auto i = 0u; i < iters; i++) {
@@ -134,6 +195,52 @@ BENCHMARK_RELATIVE(JoinHashTable_10k_value_string, iters) {
     }
     for (int64_t j = 0; j < kData; j++) {
       auto found = t.find(Value(std::to_string(j)));
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(std_10k_list, iters) {
+  constexpr int64_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    std::unordered_map<List, std::vector<const List*>> t;
+    t.reserve(kData);
+    for (int64_t j = 0; j < kData; j++) {
+      t[List({Value(j), Value(std::to_string(j))})].emplace_back(nullptr);
+    }
+    for (int64_t j = 0; j < kData; j++) {
+      auto found = t.find(List({Value(j), Value(std::to_string(j))}));
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
+BENCHMARK_RELATIVE(F14_10k_list, iters) {
+  constexpr int64_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    folly::F14FastMap<Value, std::vector<const List*>> t;
+    t.reserve(kData);
+    for (int64_t j = 0; j < kData; j++) {
+      t[List({Value(j), Value(std::to_string(j))})].emplace_back(nullptr);
+    }
+    for (int64_t j = 0; j < kData; j++) {
+      auto found = t.find(List({Value(j), Value(std::to_string(j))}));
+      folly::doNotOptimizeAway(found);
+    }
+  }
+}
+
+BENCHMARK_RELATIVE(JoinHashTable_10k_list, iters) {
+  constexpr int64_t kData = 10000;
+  for (auto i = 0u; i < iters; i++) {
+    JoinHashTable<Value, std::vector<const List*>> t(kData);
+    for (int64_t j = 0; j < kData; j++) {
+      t[List({Value(j), Value(std::to_string(j))})].emplace_back(nullptr);
+    }
+    for (int64_t j = 0; j < kData; j++) {
+      auto found = t.find(List({Value(j), Value(std::to_string(j))}));
       folly::doNotOptimizeAway(found);
     }
   }
